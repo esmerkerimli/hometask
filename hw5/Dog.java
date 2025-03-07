3f4d911bc875c58a73593e68482aeb9b4ed9af31
esmerkerimli <asmar.karimli.std@bhos.edu.az>
2025-03-08 00:26:59 +0400

    Update Dog.java

--- hw5/Dog.java
+++ hw5/Dog.java
@@ -1,16 +1,14 @@
-public class Dog extends Pet implements Foulable {
-    public Dog(String nickname, int age, int trickLevel, String[] habits){
-        super(nickname, age, trickLevel, habits);
+public class Dog extends Pet{
+    public Dog(){
+        super();
         this.species = Species.DOG;
     }
-
     @Override
-    public void respond() {
-        System.out.println("The dog barks.");
+    public void respond(){
+        System.out.println("Hav hav");
     }
 
-    @Override
-    public void foul() {
-        System.out.println("The dog digs a hole.");
+    public void foul(){
+        System.out.println("Barks loudly");
     }
 }
